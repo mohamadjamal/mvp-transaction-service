6e8e9133cc646181ca247d70864ef40ba914bae2
Mohamad Jamal AJ <mohamadjamal.aj@cognizant.com>
2020-07-14 09:02:04 +0530

    updated cicd

--- ci/setup-pipeline.sh
+++ ci/setup-pipeline.sh
@@ -4,4 +4,12 @@ fly -t main login -c <Concourse URL> -u test -p test
 
 ## Setup pipeline
 
-fly -t main set-pipeline -c pipeline.yml -p mvp-transaction-service-pipe
+fly -t main sp -c pipeline.yml -p mvp-transaction-service-pipe -l credentials.yml
+or
+fly -t main set-pipeline --config pipeline.yml --pipeline mvp-transaction-service-pipe --load-vars-from credentials.yml
+ 
+-t = --target
+-c = --config
+-p = --pipeline
+-l = --load-vars-from
+sp = set-pipeline

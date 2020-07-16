fa21c07008bf3747ae246ff2b71380dc0f049f3a
Mohamad Jamal AJ <mohamadjamal.aj@cognizant.com>
2020-07-16 15:33:48 +0530

    initial commit

--- src/main/java/com/demo/mvp/transactions/TransactionServiceApplication.java
+++ src/main/java/com/demo/mvp/transactions/TransactionServiceApplication.java
@@ -4,17 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableJpaAuditing
 @ComponentScan("com.demo.mvp")
-public class Application {
+public class TransactionServiceApplication {
 
 	private static final String ALLOWED_ORIGIN = "*";
-
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(TransactionServiceApplication.class, args);
 	}
 
 	@Bean
---
+++ src/test/java/com/demo/mvp/transactions/TransactionServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.demo.mvp.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles(profiles = "test")
+class TransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
